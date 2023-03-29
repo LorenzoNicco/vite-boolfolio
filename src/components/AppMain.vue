15dd7cadccece40b258df01c1455e3d63d415eeb
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-29 20:51:08 +0200

    Add stampa in console dei dati con chiamata axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,22 @@
 <script>
+import axios from 'axios';
 export default {
     name: "AppMain",
     data() {
         return {
         };
     },
+    created () {
+        axios.get('http://127.0.0.1:8000/api/projects')
+        .then(response => {
+            console.log(response.data);
+        })
+    }
 };
 </script>
 
 <template>
-
+    <h1>Parte Frontend</h1>
 </template>
 
 <style scoped>
